81e298b697e2ab14f83ea5ff449b73a97be3866e
Marco Zanrosso <mzanrosso5@gmail.com>
2020-03-21 21:08:34 +0100

    Project init

---
+++ Main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Covid Graphics</title>
+</head>
+<body>
+    <nav>
+    
+    </nav>
+    <div class="contenitore">
+    
+    </div>
+    <footer>
+    
+    </footer>
+</body>
+</html>
